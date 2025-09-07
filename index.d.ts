6d089ced6edd297249a95cdcd9cf5728c3cac79f
Victor Alves Faria <victoralvesfarias2004@gmail.com>
2025-09-07 01:32:56 -0300

    Hotfix

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for Markdown-Parser
+// Project: https://github.com/VictorAlvesFarias/Markdown-Parser
+// Definitions by: Victor Alves Farias
+
+export * from './src/parser/markdown-parser';
+export * from './src/parser/markdown-element';
+export * from './src/parser/markdown-element-type';
+export * from './src/utils/formatter';
+export * from './src/utils/markdown-utils';
+// Add more exports as needed for your public API
